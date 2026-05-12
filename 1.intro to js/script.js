c159afb8541e80cb3c5208637cddfd9a88fd794c
prince singh <princesingh36006457@gmail.com>
2026-05-12 11:25:05 +0530

    Add files via upload

---
+++ 1.intro to js/script.js
@@ -0,0 +1,28 @@
+//  let name="Prince";
+//  let age=18;
+//  let a =28;
+//  let b=12;
+//  let c=a+b;
+//  console.log("My name is "+name);
+//  console.log("My age is",age);
+//  console.log(c);
+//  console.log(c-5);
+
+// let varName="prince";
+// console.log(varName);
+
+// varName=18;
+// console.log(varName);
+
+
+// const college="BGIEM";
+// // college="SRIT";
+// console.log(college);
+
+// var college="BGIEM";
+// college="SRIT";
+// console.log(college);
+
+let varName;
+varName="Prince";
+console.log(varName);
