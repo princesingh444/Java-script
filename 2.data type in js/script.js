4080a4b19ddd0b01cd3d060223b96b87d2574439
prince singh <princesingh36006457@gmail.com>
2026-05-13 10:46:21 +0530

    Add files via upload

---
+++ 2.data type in js/script.js
@@ -0,0 +1,40 @@
+// let a=123;
+// a="prince";
+// a='123';
+// a=true;
+// a=false;
+
+// let b;
+// let sum=null;
+
+// console.log ("The value of sum is",sum);
+// console.log ("The type of sum is",typeof(sum));
+
+
+// let v=12;
+// console.log(v+1);
+
+// let v1='12';
+// console.log(v1+1);
+
+// let abc=123;
+// let result=String(abc);
+
+// console.log("The value of abc is",abc);
+// console.log("The type of abc is",typeof(abc));
+// console.log("The type of result is",typeof(result));
+
+// let name="123";
+// name=Number(name);
+
+
+// console.log('The value of name is',name);
+// console.log("The type of name is",typeof(name));
+
+
+// let name=12;
+// name=Boolean(name);
+
+
+// console.log('The value of name is',name);
+// console.log("The type of name is",typeof(name));
