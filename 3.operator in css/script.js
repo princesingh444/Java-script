6e3cf2c66f015f8d5f027af023d30349cf5b418f
prince singh <princesingh36006457@gmail.com>
2026-05-14 10:00:30 +0530

    Add files via upload

---
+++ 3.operator in css/script.js
@@ -0,0 +1,87 @@
+// Arthematic Operator
+
+// let a=8;
+// let b=4;
+// let sum=a+b;
+// let sub=a-b;
+// let mul=a*b;
+// let div=a/b;
+// let mod=a%b;
+// console.log("The sum is",sum);
+// console.log("The sub is",sub);
+// console.log("The mul is",mul);
+// console.log("The div is",div);
+// console.log("The mod is",mod);
+
+
+// Incerment Operator 
+
+// let p=33;
+// p++;
+// console.log(p);
+
+// Decerment Operator 
+
+//  let q=27;
+//  console.log(q);
+//  console.log(q--);
+//  console.log(q );
+ 
+ 
+
+
+// Pre Increment Operator 
+
+// let a=33;
+// ++a;
+// console.log(a);
+
+// Pre Decrement Operator 
+// let b=33;
+// --b;
+// console.log(b);
+
+
+// Exponention Operator 
+
+// let a=3;
+// let b=2;
+// console.log(a**b);
+
+// Short hand Operartor
+
+// let a=3;
+// let b=5;
+// // a=a+b;
+// a+=b;
+// console.log(a);
+
+
+// Assignment (=)
+// equal to (==)
+// Strict equals (===)
+
+// let a=5;
+// let b=3;
+// console.log("Equals to-",a==b);
+// console.log("Strict equals -",a===b);
+
+// console.log(a<b);
+
+
+// Logical AND Operator (&&)
+
+// let a=true;
+// let b=false;
+// console.log(a&&b);
+
+// Logical OR Opearator (||)
+
+// let a=false;
+// let b=true;
+// console.log(a||b);
+
+// Lofical NOT Operator (!)
+
+// let a=false;
+// console.log(!a);
