649a01e0a7b203a27e77977b951d10ea6bd658fb
prince singh <princesingh36006457@gmail.com>
2026-05-15 09:57:51 +0530

    Add files via upload

---
+++ 4.if-else in js/script.js
@@ -0,0 +1,20 @@
+// let marks=55;
+// if(marks>=90){
+//     console.log("A+");
+// }else if(marks>=80){
+//     console.log("A");
+// }
+// else if(marks>=70){
+//     console.log("B+");
+// }else if(marks>=60){
+//     console.log("B");
+// }else{
+//     console.log("not good focus on study !!!");
+    
+// }
+
+// Tersary Operator 
+
+let age=14;
+age>=18 ? console.log("Eligible to vote ") : console.log("not eligible to vote");
+ 
