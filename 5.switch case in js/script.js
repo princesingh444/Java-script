407b1a45a67fe7f096fdbe09216952136a8983a4
prince singh <princesingh36006457@gmail.com>
2026-05-16 13:31:01 +0530

    Add files via upload

---
+++ 5.switch case in js/script.js
@@ -0,0 +1,20 @@
+let marks=80;``
+ switch (marks){
+    case 90:
+        console.log("A+");
+        break;
+    case 80:
+        console.log("A");
+        break;
+
+    case 70:
+        console.log("B+");
+        break;90
+    case 60:
+         console.log("B");
+         break;
+        default:
+            console.log("fail")
+            break
+        }
+ 
