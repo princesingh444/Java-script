7081c82ac3ea8e473319678f37207a6d20b93935
prince singh <princesingh36006457@gmail.com>
2026-05-17 11:13:23 +0530

    Add files via upload

---
+++ 6.loops in js/script.js
@@ -0,0 +1,23 @@
+// For loop 
+
+// for(let i=0;i<100;i++){
+//     console.log("Prince ",i);    
+// }
+
+
+// while loop
+// let i=0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// Do while loop
+
+
+// let i=0;
+// do {
+//     console.log(i);
+//     i++;
+    
+// } while (i<=10);
